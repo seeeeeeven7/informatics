36d97700dcd8541f7d8740ce6e7fa6ac8d1c5dc8
羊羽 <seeeeeeven7@users.noreply.github.com>
2023-11-04 21:27:49 +0800

    AtCoder / abc327

---
+++ AtCoder/abc327/E_make.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("E1.in", "w", stdout);
+	int n = 5000;
+	cout << 5000 << endl;
+	for (int i = 0; i < n; i++) cout << i + 1 << ' ';
+}
--- clean.bat
+++ clean.bat
@@ -2,3 +2,10 @@
 for /r %%i in (*.exe) do (
     del "%%i"
 )
+for /r %%i in (*.in) do (
+    del "%%i"
+)
+for /r %%i in (*.out) do (
+    del "%%i"
+)
+
